44721f0fa32960afb022668af7a29affdb2d156e
hoadt <hoa.dt@zinza.com.vn>
2023-09-03 14:39:49 +0700

    temp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,14 @@ class HomeController < ApplicationController
 
   end
 
-  def register
+  def posts
+    post = Post.new(permit_params)
+    if post.save
 
+    end
+  end
+
+  def permit_params
+    params.permit(:name, :phone, :message)
   end
 end
---
+++ db/migrate/20230902120752_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :phone
+      t.string :message
+
+      t.timestamps
+    end
+  end
+end
